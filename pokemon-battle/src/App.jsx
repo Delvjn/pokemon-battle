1ab0fca3782e06d58a91f676cbeae3dd3a1d3bf0
Mats Haugum <mats.haugum@gmail.com>
2026-04-07 15:21:27 +0200

    axios, getById, battlepage, router

--- pokemon-battle/src/App.jsx
+++ pokemon-battle/src/App.jsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import BattlePage from './pages/battlePage'
 
 function App() {
   return (
     <>
-      
+      <Routes>
+        <Route path="/battle" element={<BattlePage />}/>
+      </Routes>
     </>
   )
 }
